ce40f51fae8800dae2a6d5e047c0344700882953
zyc2478 <zyc2478@126.com>
2018-10-10 23:16:59 +0800

    Springboot项目初始化后，第一次提交到本地仓库

---
+++ src/main/java/com/zyc/job_manager/JobManagerApplication.java
@@ -0,0 +1,12 @@
+package com.zyc.job_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobManagerApplication.class, args);
+    }
+}
